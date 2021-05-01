610a47b7c28f31f0f021c1d734c6e87ae794b60e
whisky96 <37628752+whisky96@users.noreply.github.com>
2021-05-01 15:05:09 +0200

    Update telegram.py
    La lista d utenti viene gestista da un json , dove tramite il login e logout viene manipolato il suo stato d attivita (on/off).
    Se è un utente nuovo e non viene trovato si dovra registrare.
    Una volta fatta l autentificaione, l utente puo fare il check degli ambienti

--- telegram.py
+++ telegram.py
@@ -10,23 +10,63 @@ import json
  
 TOKEN="" #da sostituire
  
+def logout(name):
+    users=json.load(open('users.json'))
+    for us in users['Users']:
+            if us['username'] == name:
+                us['status']= 'off'
+                json.dump(users,open('users.json','w'))   
+
+def status(name):
+    users=json.load(open('users.json'))['Users']
+    for us in users:
+         if us['username']==name and us['status']== "on":
+             return True
+    return False
+
+def register(name,pssw):
+    users=json.load(open('users.json'))
+    us={"username":name,"password":pssw,"status":"off"}
+    users["Users"].append(us)
+    json.dump(users,open('users.json','w'))  
+
+def login(chat_id,name,pssw):
+    users=json.load(open('users.json'))
+    for us in users['Users']:
+            if us['username'] == name and us['password']==pssw :
+                us['status']= 'on'
+                bot.sendMessage(chat_id,"sei registrato")
+                json.dump(users,open('users.json','w'))   
+                return True
+    bot.sendMessage(chat_id,"errore nella compilazione")
+    return False
+
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     if content_type=='text':
-         chat_id=msg['chat']['id']
-         command=msg['text']
-         if command=='/start':
-             bot.sendMessage(chat_id, 'Ciao utente cosa vuoi? \n') 
-         elif command=='/registrati':
-             bot.sendMessage(chat_id, 'ti sei registrato nella stanza numero 25\n') 
-         elif  command=='/dati':
-             keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                 [InlineKeyboardButton(text='Temperatura', callback_data='temperatura')],
-                 [InlineKeyboardButton(text='C02', callback_data='c02')],
-                 [InlineKeyboardButton(text='Umidita', callback_data='umidita')]],)
-             bot.sendMessage(chat_id, 'Cosa vuoi?', reply_markup=keyboard)
-         else :
-            bot.sendMessage(chat_id, 'unico comando che conosco è /start e /dati \n')
+        chat_id=msg['chat']['id']
+        name=msg["from"]["username"]
+        txt=msg['text']
+        bot.sendMessage(chat_id,'ciao caro {}'.format(name))
+        if txt.startswith('/login'):
+            params = txt.split()[1:]
+            login(chat_id,params[0],params[1])
+        elif txt.startswith('/register'):
+            params = txt.split()[1:]
+            register(chat_id,params[0],params[1])
+        elif txt.startswith('/check') and status(name):
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text='Temperatura', callback_data='temperatura')],
+                [InlineKeyboardButton(text='C02', callback_data='c02')],
+                [InlineKeyboardButton(text='Umidita', callback_data='umidita')]],)
+            bot.sendMessage(chat_id, 'sei on , puoi controllare', reply_markup=keyboard)
+        elif txt.startswith('/logout'):
+            logout(name)
+            bot.sendMessage(chat_id,"sei uscito")
+        else:
+            bot.sendMessage(chat_id,"comando non riconosciuto, l unici comandi che son disponibili sono /login /logout /check /register")
+        #elif txt.startwith('/register'):
+        #    register()
 
 def on_callback_query(msg): # come premo ritornano cose 
     query_id, chat_ID, query_data = telepot.glance(msg, flavor='callback_query')
@@ -41,5 +81,6 @@ def on_callback_query(msg): # come premo ritornano cose
 bot = telepot.Bot(TOKEN)
 MessageLoop(bot, {'chat':on_chat_message, 'callback_query':on_callback_query}).run_as_thread()
 print('Listening ...')
+
 while 1:
     time.sleep(10)
