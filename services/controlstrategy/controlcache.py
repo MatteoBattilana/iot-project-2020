65ec92a69b57e0d9546b54ddf797005fde4353ee
Andrea <s280055@studenti.polito.it>
2021-05-04 22:57:06 +0200

    Removed thread approach in control cache

--- services/controlstrategy/controlcache.py
+++ services/controlstrategy/controlcache.py
@@ -12,30 +12,6 @@ class ControlCache(threading.Thread):
         self._cache = []
         self.lock = threading.Lock()
         self._time_interval = time_interval
-        self._run = True
-    
-    def run(self):
-        while self._run:
-            for cache in self._cache:
-                if len(cache["temperature"]) != 0:
-                    first_temp = cache["temperature"][0]["timestamp"]
-                    last_temp = cache["temperature"][-1]["timestamp"]
-                    if (last_temp-first_temp)/60 > self._time_interval:
-                        self.popCache(cache["groupId"], "temperature")
-                if len(cache["humidity"]):
-                    first_hum = cache["humidity"][0]["timestamp"]
-                    last_hum = cache["humidity"][-1]["timestamp"]
-                    if (last_hum-first_hum)/60 > self._time_interval:
-                        self.popCache(cache["groupId"], "humidity")
-                if len(cache["co2"]):
-                    first_co2 = cache["co2"][0]["timestamp"]
-                    last_co2 = cache["co2"][-1]["timestamp"]
-                    if (last_co2-first_co2)/60 > self._time_interval:
-                        self.popCache(cache["groupId"], "co2")
-            time.sleep(1)
-            
-    def stop(self):
-        self._run = False
 
     def createCache(self, groupId):
         new_cache = {
@@ -44,9 +20,10 @@ class ControlCache(threading.Thread):
             "humidity":[],
             "co2":[]
         }
-        self.lock.acquire()
         
         fields = []
+
+        self.lock.acquire()
 
         uri = "http://localhost:8090/channel/"+groupId+"/feeds/getMinutesData?minutes="+str(self._time_interval)
         logging.debug(f"Call request to uri = {uri}")
@@ -62,6 +39,7 @@ class ControlCache(threading.Thread):
                         new_cache[field].append(feed["field"]+str(i))
         except Exception as e:
             logging.error(f"Request Error {e} for uri={uri}")
+        
         self._cache.append(new_cache)
         self.lock.release()
     
@@ -75,6 +53,23 @@ class ControlCache(threading.Thread):
                     "timestamp":timestamp
                 }
                 cache[measuretype].append(to_append)
+
+        for cache in self._cache:
+                if len(cache["temperature"]) != 0:
+                    first_temp = cache["temperature"][0]["timestamp"]
+                    last_temp = cache["temperature"][-1]["timestamp"]
+                    if (last_temp-first_temp)/60 > self._time_interval:
+                        self.popCache(cache["groupId"], "temperature")
+                if len(cache["humidity"]):
+                    first_hum = cache["humidity"][0]["timestamp"]
+                    last_hum = cache["humidity"][-1]["timestamp"]
+                    if (last_hum-first_hum)/60 > self._time_interval:
+                        self.popCache(cache["groupId"], "humidity")
+                if len(cache["co2"]):
+                    first_co2 = cache["co2"][0]["timestamp"]
+                    last_co2 = cache["co2"][-1]["timestamp"]
+                    if (last_co2-first_co2)/60 > self._time_interval:
+                        self.popCache(cache["groupId"], "co2")
         
         self.lock.release()
 
