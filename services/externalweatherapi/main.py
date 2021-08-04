c8b4f21e82716e62cf1811d087d126be447f4906
Andrea <s280055@studenti.polito.it>
2021-08-04 12:04:07 +0200

    Updated old modification to external weather api files

--- services/externalweatherapi/main.py
+++ services/externalweatherapi/main.py
@@ -1,6 +1,7 @@
 # Path hack.
 import sys, os
-
+import datetime
+from datetime import timedelta
 import requests
 sys.path.insert(0, os.path.abspath('..'))
 from commons.ping import *
@@ -56,6 +57,10 @@ class ExternalWeatherApi():
                 return json.dumps(self._getForecastWeather(params['lat'], params['lon'], self._openweatherapikey, params['minutes'], params['hours'], params['days']), indent=4)
             elif uri[0] == "forecastWeatherStatus" and "lat" in params and "lon" in params:
                 return json.dumps(self._getForecastWeather(params['lat'], params['lon'], self._openweatherapikey), indent=4)
+            elif uri[0] == "forecastPollution" and "lat" in params and "lon" in params:
+                return json.dumps(self._getForecastAirPollution(params['lat'], params['lon']), indent=4)
+            elif uri[0] == "tomorrowPollution" and "lat" in params and "lon" in params:
+                return json.dumps(self.whenToOpen(params['lat'], params['lon']), indent=4)
             else:
                 cherrypy.response.status = 404
                 return json.dumps({"error":{"status": 404, "message": "Invalid request"}}, indent=4)
@@ -83,40 +88,84 @@ class ExternalWeatherApi():
         days = int(days)
         if r.status_code == 200:
             #logging.debug(r.json())
-            if minutes <= 60 and hours <= 48:
+            if minutes <= 60 and hours <= 48 and days <= 7:
                 for i in range(0, minutes):
-                    retInformation["minutes"].append(
-                        {
-                        "timestamp":r.json()["minutely"][i]["dt"],
-                        "precipitations":r.json()["minutely"][i]["precipitation"]
-                    }) 
+                    if "minutely" in r.json():
+                        retInformation["minutes"].append(
+                            {
+                            "timestamp":r.json()["minutely"][i]["dt"],
+                            "precipitations":r.json()["minutely"][i]["precipitation"]
+                        }) 
                 for j in range(0, hours):
-                    retInformation["hours"].append(
-                        {
-                            "timestamp":r.json()["hourly"][j]["dt"],
-                            "temperature":float(r.json()["hourly"][j]["temp"])-273.15,
-                            "pressure":r.json()["hourly"][j]["pressure"],
-                            "humidity":r.json()["hourly"][j]["humidity"],
-                            "wind_speed":r.json()["hourly"][j]["wind_speed"]
-
-                        }
-                    )
+                    if "hourly" in r.json():
+                        retInformation["hours"].append(
+                            {
+                                "timestamp":r.json()["hourly"][j]["dt"],
+                                "temperature":float(r.json()["hourly"][j]["temp"])-273.15,
+                                "pressure":r.json()["hourly"][j]["pressure"],
+                                "humidity":r.json()["hourly"][j]["humidity"],
+                                "wind_speed":r.json()["hourly"][j]["wind_speed"]
+                            }
+                        )
                 for i in range(0, days):
-                    retInformation["days"].append(
-                       {
-                            "timestamp":r.json()["daily"][i]["dt"],
-                            "morningTemp":float(r.json()["daily"][i]["temp"]["morn"])-273.15,
-                            "dayTemp":float(r.json()["daily"][i]["temp"]["day"])-273.15,
-                            "eveningTemp":float(r.json()["daily"][i]["temp"]["eve"])-273.15,
-                            "nightTemp":float(r.json()["daily"][i]["temp"]["night"])-273.15,
-                            "minDailyTemp":float(r.json()["daily"][i]["temp"]["min"])-273.15,
-                            "maxDailyTemp":float(r.json()["daily"][i]["temp"]["max"])-273.15
-                        } 
-                    )
+                    if "daily" in r.json():
+                        retInformation["days"].append(
+                           {
+                                "timestamp":r.json()["daily"][i]["dt"],
+                                "morningTemp":float(r.json()["daily"][i]["temp"]["morn"])-273.15,
+                                "dayTemp":float(r.json()["daily"][i]["temp"]["day"])-273.15,
+                                "eveningTemp":float(r.json()["daily"][i]["temp"]["eve"])-273.15,
+                                "nightTemp":float(r.json()["daily"][i]["temp"]["night"])-273.15,
+                                "minDailyTemp":float(r.json()["daily"][i]["temp"]["min"])-273.15,
+                                "maxDailyTemp":float(r.json()["daily"][i]["temp"]["max"])-273.15
+                            } 
+                        )
             else:
                 logging.error(f"Specified parameters cannot accepted by the openweatherapi")
         else:
             logging.error(f"Unable to contact openweathermap")
+        return retInformation
+
+    def whenToOpen(self, lat, lon):
+        #we know the temperature and pressure for the next 48 hours
+        #then we have the measures of the next 7 days of 
+        #min and max temperatures
+        #and also morning, day, evening and night temperatures
+        #--> i want to know tomorrow when it will be the best moment to open the windows
+        #based on the forecasted value of temperature and humidity
+        #and if possible also on the last week air pollution data
+        tomorrow_pollution = []
+        today = datetime.datetime(datetime.date.today().year,datetime.date.today().month,datetime.date.today().day)
+        tomorrow_start = today + timedelta(days=1)
+        tomorrow_end = tomorrow_start + timedelta(hours=23, minutes=59, seconds=59)
+        tomorrow_start_timestamp = datetime.datetime.timestamp(tomorrow_start)
+        tomorrow_end_timestamp = datetime.datetime.timestamp(tomorrow_end)
+        _weather_data = self._getForecastWeather(lat, lon, self._openweatherapikey, days=1)
+        _pollution_data = self._getForecastAirPollution(lat, lon)
+        for data in _pollution_data["pollution_values"]:
+            if data["timestamp"] > tomorrow_start_timestamp and data["timestamp"] < tomorrow_end_timestamp:
+                tomorrow_pollution.append(data)
+        return tomorrow_pollution
+    def _getForecastAirPollution(self, lat, lon):
+        retInformation = {
+            "pollution_values":[]
+        }
+        uri = "http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat="+lat+"&lon="+lon+"&appid="+self._openweatherapikey
+        try:
+            r = requests.get(uri)
+            if r.status_code == 200:
+                if "list" in r.json() and "components" in r.json()["list"][0]:
+                    for data in r.json()["list"]:
+                        retInformation["pollution_values"].append(
+                            {
+                                "timestamp":data["dt"],
+                                 **data["components"]}
+                                 )
+            else:
+                logging.error("Unable to get temperature from openweathermap: " + json.dumps(r.json()))
+        except Exception as e:
+            logging.error(f"GET request went wrong ")
+        
         return retInformation
         
 def _getCurrentWeatherStatus(lat, lon, safeWindSpeed, openweatherapikey):
@@ -129,7 +178,7 @@ def _getCurrentWeatherStatus(lat, lon, safeWindSpeed, openweatherapikey):
             retInformation["temperature"] = r1.json()["main"]["temp"]
             retInformation["humidity"] = r1.json()["main"]["humidity"]
         else:
-            logging.error("Unable to get temperature from openweatermap: " + json.dumps(r.json()))
+            logging.error("Unable to get temperature from openweathermap: " + json.dumps(r.json()))
 
         # Refer to https://openweathermap.org/weather-conditions
         if "weather" in r1.json() and (int(r1.json()["weather"][0]["id"]) < 800 or float(r1.json()["wind"]["speed"]) > safeWindSpeed) :
@@ -139,16 +188,16 @@ def _getCurrentWeatherStatus(lat, lon, safeWindSpeed, openweatherapikey):
 
 
     else:
-        logging.error("Unable to contact openweatermap")
+        logging.error("Unable to contact openweathermap")
 
     r2 = requests.get("http://api.openweathermap.org/data/2.5/air_pollution?lat=" + lat + "&lon=" + lon + "&units=metric&appid=" + openweatherapikey)
     if r2.status_code == 200:
         if "list" in r2.json() and "components" in r2.json()["list"][0]:
             retInformation = {**retInformation, **r2.json()["list"][0]["components"]}
         else:
-            logging.error("Unable to get temperature from openweatermap: " + json.dumps(r.json()))
+            logging.error("Unable to get temperature from openweathermap: " + json.dumps(r.json()))
     else:
-        logging.error("Unable to contact openweatermap")
+        logging.error("Unable to contact openweathermap")
 
     return retInformation
     
@@ -173,6 +222,12 @@ if __name__=="__main__":
                     "uri":"/forecastWeatherStatus",
                     "version": 1,
                     "parameter": [{"name": "lat", "unit":"float"}, {"name": "lon", "unit":"float"}, {"name": "minutes", "unit":"int"}, {"name": "hours", "unit":"int"}, {"name": "days", "unit":"int"}]
+                },
+                {
+                    "type":"web",
+                    "uri":"/forecastPollution",
+                    "version": 1,
+                    "parameter": [{"name":"lat", "unit":"float"}, {"name":"lon ", "unit":"float"}]
                 }
                 ,{
                         "type": "web",
